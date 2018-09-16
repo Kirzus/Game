12a43d3baaa74bd20a930cdd5103be6bdba6c590
Kirzus <arnaud.malanda@gmail.com>
2018-09-16 23:03:10 +0200

    [Update][JS] - Working display of rgb (style TypeError to fix)

--- RGB Color Game/colorGame.js
+++ RGB Color Game/colorGame.js
@@ -1,21 +1,41 @@
-var red = 0;
-var green = 0;
-var blue = 0;
+// $(document).ready(function() {
+    //Storing color variables
+    var rgb = "";
+    //Storing dom queries
+    var squares = document.querySelectorAll(".square");
+    var displayRgb = document.querySelector("#rgb");
+    // Randomize the square holding the correct color
 
-var rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
-var squares = document.querySelectorAll(".square")
+    var randomRgb = Math.floor(Math.random() * Math.floor(6));
+    var correctRgb = squares[randomRgb];
 
-squares.forEach(function (element) {
-    randomColor(255);
-    element.style.background = rgb;
-    // console.log(rgb);
-    // element[index].style.background(rgb);
-});
+    console.log(randomRgb);
 
-function randomColor(max) {
-    red = Math.floor(Math.random() * Math.floor(max));
-    green = Math.floor(Math.random() * Math.floor(max));
-    blue = Math.floor(Math.random() * Math.floor(max));
-    rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
-    return rgb;
-}
+    // squares.forEach(function (element) {
+    //     randomColor(255);
+    //     element.style.background = rgb;
+    // });
+
+    for (i = 0; i <= squares.length; i++ ) {
+        randomColor(255);
+        // squares[i].setAttribute("style", rgb);
+        squares[i].style.background = rgb;
+    }
+
+    function randomColor(max) {
+        var red = Math.floor(Math.random() * Math.floor(max));
+        var green = Math.floor(Math.random() * Math.floor(max));
+        var blue = Math.floor(Math.random() * Math.floor(max));
+        rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
+        updateContent();
+        return rgb;
+    }
+
+    function updateContent() {
+        displayRgb.textContent = correctRgb.style.background;
+    }
+
+    function randomCorrect(number) {
+        return Math.floor(Math.random() * Math.floor(number));
+    }
+// });
